2bad1ad64846e6aabc4a33d79a73b50a54874cb9
Andres Garcia <andres_uchiha2000@hotmail.com>
2023-11-27 17:08:35 -0600

    Cambios

---
+++ javascript/Operacion.js
@@ -0,0 +1,8 @@
+function suma(){
+    let t1cd1=document.getElementById("t1cd1").value;
+    let t1nm1=document.getElementById("t1mn1").value;
+    let t1dg1=document.getElementById("t1dg1").value;
+    let t1cr1=document.getElementById("t1cr1").value;
+    let t1pt1=t1dg1+t1cr1
+    document.getElementById('t1pt1').value=t1pt1
+}
